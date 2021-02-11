573b5c0ff019ba9e32d002a953a05f164a549906
Victor Lobato <victorsilva123190@gmail.com>
2021-02-11 08:05:50 -0300

    Projeto criado

---
+++ src/main/java/com/pv/cursoimc/CursoimcApplication.java
@@ -0,0 +1,13 @@
+package com.pv.cursoimc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoimcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoimcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pv/cursoimc/CursoimcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pv.cursoimc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoimcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
